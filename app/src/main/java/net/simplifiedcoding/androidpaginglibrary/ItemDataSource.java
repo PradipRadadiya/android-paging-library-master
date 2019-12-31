cb5587733b81d354d1db3fb688317a3dc9283e07
G-Pradip <pradip.radadiya@grapes-solutions.com>
2019-12-31 16:20:36 +0530

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/androidpaginglibrary/ItemDataSource.java
@@ -0,0 +1,84 @@
+package net.simplifiedcoding.androidpaginglibrary;
+
+import android.arch.paging.PageKeyedDataSource;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ItemDataSource extends PageKeyedDataSource<Integer, Item> {
+
+    public static final int PAGE_SIZE = 50;
+    private static final int FIRST_PAGE = 1;
+    private static final String SITE_NAME = "stackoverflow";
+
+    @Override
+    public void loadInitial(@NonNull LoadInitialParams<Integer> params, @NonNull final LoadInitialCallback<Integer, Item> callback) {
+        Log.e("page size", "------------" + FIRST_PAGE);
+        RetrofitClient.getInsance()
+                .getApi()
+                .getAnswers(FIRST_PAGE, PAGE_SIZE, SITE_NAME)
+                .enqueue(new Callback<StackApiResponse>() {
+                    @Override
+                    public void onResponse(@NonNull Call<StackApiResponse> call, Response<StackApiResponse> response) {
+                        if (response.body() != null) {
+                            callback.onResult(response.body().items, null, FIRST_PAGE + 1);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<StackApiResponse> call, Throwable t) {
+
+                    }
+                });
+    }
+
+    @Override
+    public void loadBefore(@NonNull final LoadParams<Integer> params, @NonNull final LoadCallback<Integer, Item> callback) {
+        Log.e("page size", "------------" + params.key);
+        RetrofitClient.getInsance()
+                .getApi()
+                .getAnswers(params.key, PAGE_SIZE, SITE_NAME)
+                .enqueue(new Callback<StackApiResponse>() {
+                    @Override
+                    public void onResponse(Call<StackApiResponse> call, Response<StackApiResponse> response) {
+
+                        if (response.body() != null) {
+                            Integer key = (params.key > 1) ? params.key - 1 : null;
+                            callback.onResult(response.body().items, key);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<StackApiResponse> call, Throwable t) {
+
+                    }
+                });
+
+    }
+
+    @Override
+    public void loadAfter(@NonNull final LoadParams<Integer> params, @NonNull final LoadCallback<Integer, Item> callback) {
+        Log.e("page size", "------------" + params.key);
+        RetrofitClient.getInsance()
+                .getApi()
+                .getAnswers(params.key, PAGE_SIZE, SITE_NAME)
+                .enqueue(new Callback<StackApiResponse>() {
+                    @Override
+                    public void onResponse(Call<StackApiResponse> call, Response<StackApiResponse> response) {
+
+                        if (response.body() != null) {
+                            Integer key = response.body().has_more ? params.key + 1 : null;
+                            callback.onResult(response.body().items, key);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<StackApiResponse> call, Throwable t) {
+
+                    }
+                });
+    }
+}
